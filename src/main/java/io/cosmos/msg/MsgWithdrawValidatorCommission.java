1414eab22e3a7b7b54a13ef088d201da84e874e3
qzhongwood <2609611@qq.com>
2019-10-19 18:04:38 +0800

    withdraw

---
+++ src/main/java/io/cosmos/msg/MsgWithdrawValidatorCommission.java
@@ -0,0 +1,36 @@
+package io.cosmos.msg;
+
+import io.cosmos.msg.utils.Message;
+import io.cosmos.msg.utils.type.MsgWithdrawDelegatorRewardValue;
+import io.cosmos.msg.utils.type.MsgWithdrawValidatorCommissionValue;
+
+public class MsgWithdrawValidatorCommission extends MsgBase {
+
+    public static void main(String[] args) {
+        MsgWithdrawValidatorCommission msg = new MsgWithdrawValidatorCommission();
+        msg.setMsgType("cosmos-sdk/MsgWithdrawValidatorCommission");
+        String mnemonic = "sentence deputy little switch fiction balcony hollow iron net index sound hollow scare attitude only cushion best candy wonder phone napkin sketch announce derive";
+        msg.initMnemonic(mnemonic);
+
+        Message messages = msg.produceMsg("cosmosvaloper1y5cj26cexle8mrpxfksnly2djzxx79zq2mf083");
+
+        msg.submit(messages,
+                "stake",
+                "6",
+                "200000",
+                "testchain",
+                "cosmos withdraw");
+    }
+
+    public Message produceMsg(String validatorAddr) {
+
+        MsgWithdrawValidatorCommissionValue value = new MsgWithdrawValidatorCommissionValue();
+        value.setValidatorAddress(validatorAddr);
+
+        Message<MsgWithdrawValidatorCommissionValue> msg = new Message<>();
+        msg.setType(msgType);
+        msg.setValue(value);
+        return msg;
+    }
+
+}
